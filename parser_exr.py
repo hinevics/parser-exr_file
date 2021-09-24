dd89b106be4df25c68f89d21f1043dc2e8d095f0
Sasha Khinevich <hinevics@gmail.com>
2021-09-24 14:24:33 +0300

    creat test version prog

--- parser_exr.py
+++ parser_exr.py
@@ -1,15 +1,24 @@
 import re
 
+DEFUALT_PATH_FILE = r'E:\Hinevics\Dropbox\Programm\GitHub\parser-exr_file\CUBE___1312.exr'
 
 def search_comment(fb:bytes) -> str:
+    comment = re.search(pattern=r'comment(?P<comment>.+)comments(?P<comments>.+)compression', string=str(fb))
+    print(re.sub(string=comment.groupdict()['comment'], pattern=r'\\x00string\\x00\\t\\x00\\x00\\x00', repl=''))
+    # print(re.sub(pattern=r'', )))
+    # print(re.search(pattern=r'\\x00string\\x00\\t\\x00\\x00\\x00', string=comment.groupdict()['comment']).group())
 
-def parser():
-    pass
+
+def openfile(path:str=DEFUALT_PATH_FILE) -> bytes:
+    with open(file=path, mode='br') as file:
+        r_0 = file.readline()
+    return r_0
+   
 
 def main():
-    parser()
+    fb = openfile(path=DEFUALT_PATH_FILE)
+    search_comment(fb)
 
 
-
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
