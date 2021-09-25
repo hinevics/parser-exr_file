540c11b3d16978cb5fc0777ecb4474643d7e01b5
Sasha Khinevich <hinevics@gmail.com>
2021-09-25 22:22:29 +0300

    open file in this dir

--- parser_exr.py
+++ parser_exr.py
@@ -2,7 +2,7 @@ import re
 import os
 
 
-DEFUALT_PATH_FILE = r'D:\Development\Coding\parser-exr_file\CUBE___1312.exr'
+DEFAULT_PATH_FILE = r'D:\Development\Coding\parser-exr_file\CUBE___1312.exr'
 
 def search_comment(fb:bytes) -> str:
     comment = re.search(pattern=r'comment(?P<comment>.+)comments(?P<comments>.+)compression', string=str(fb))
@@ -11,7 +11,7 @@ def search_comment(fb:bytes) -> str:
     # print(re.search(pattern=r'\\x00string\\x00\\t\\x00\\x00\\x00', string=comment.groupdict()['comment']).group())
 
 
-def openfile(path:str=DEFUALT_PATH_FILE) -> bytes:
+def openfile(path:str=DEFAULT_PATH_FILE) -> bytes:
     with open(file=path, mode='br') as file:
         r_0 = file.readline()
     return r_0
@@ -20,13 +20,18 @@ def opendir() -> list:
     """
     функция находит все файлы расширения exr в директории где находится скрипт
     """
-    yield [nf for nf in os.listdir() if re.search(pattern=r'.exr$', string=nf)]
+    for nf in os.listdir():
+        if re.search(pattern=r'.exr$', string=nf):
+            yield nf
 
 
 def main():
     # fb = openfile(path=DEFUALT_PATH_FILE)
     # search_comment(fb)
-    opendir()
+    for i in opendir():
+        with open(file=r'{a1}\{a2}'.format(a2=i, a1=os.getcwd())) as file:
+            print(file)
+
 
 if __name__ == '__main__':
     main()
