93067fb4941dabc0b88d6e6426294389037c654b
Sasha Khinevich <hinevics@gmail.com>
2021-09-26 00:23:01 +0300

    update

--- parser_exr.py
+++ parser_exr.py
@@ -1,22 +1,15 @@
-import re
-import os
-
+from re import search
+from os import getcwd, listdir, rename
 
 def search_comment(fb:str) -> str:
     """
-    поиск названия
+    name search
     """
-    return re.search(pattern=r'comment.+\\x00\\x00(?P<comment>.+)comments', string=fb).group('comment')
-    # comment = re.search(pattern=r'comment(?P<comment>.+)comments(?P<comments>.+)compression', string=str(fb))
-    # print(comment.groupdict()['comment'])
-    # print(re.sub(string=comment.groupdict()['comment'], pattern=r'(\\x00string\\x00\\t\\x00\\x00\\x00)|(\\x00string\\x00\\x0c\\x00\\x00\\x00)', repl=''))
-    # print(re.sub(pattern=r'', )))
-    # print(re.search(pattern=r'\\x00string\\x00\\t\\x00\\x00\\x00', string=comment.groupdict()['comment']).group())
-
-
+    return search(pattern=r'comment.+\\x00\\x00(?P<comment>.+)comments', string=fb).group('comment')
+  
 def openfile(path:str) -> str:
     """
-    функция открывает файлы и возврщает первые биты, где хранится информация
+    the function opens files and returns the first bits where the information is stored
     """
     with open(file=path, mode='br') as file:
         r_0 = file.readline()
@@ -24,27 +17,21 @@ def openfile(path:str) -> str:
    
 def opendir() -> list:
     """
-    функция находит все файлы расширения exr в директории где находится скрипт
+    the function finds all exr files in the directory where the script is located
     """
-    for nf in os.listdir():
-        if re.search(pattern=r'.exr$', string=nf):
+    for nf in listdir():
+        if search(pattern=r'.exr$', string=nf):
             yield nf
 
 
 def main():
     for fileexr in opendir():
-        pathfile = r'{}\{}'.format(os.getcwd(), fileexr)
+        pathfile = r'{}\{}'.format(getcwd(), fileexr)
         fb = openfile(path=pathfile)
         comment = search_comment(fb=fb)
-        newname = r'{path}\{commet}'.format(path=os.getcwd(), commet=comment)
-        os.rename(src=pathfile, dst=newname)
-    # os.rename(src='{}\{}', dst='\')
-    # rename_file(path='{}\{}'.format(os.getcwd(), 'test.exr'), newname='{}\{}'.format(os.getcwd(), 'CUBE___1312.exr'))
-    # rename_file(path=r'D:\Development\Coding\parser-exr_file\CUBE___1312.exr', newname=r'D:\Development\Coding\parser-exr_file\test.exr')
-    # for i in enopendir():
-    #     fb = openfile(path=i)
-        # print(search_comment(fb=str(fb)))
-        # print(rename_file(path=i, newname=r'{a1}\{a2}'.format(a1=os.getcwd(), a2='test')))
-
+        newname = r'{path}\{commet}'.format(path=getcwd(), commet=comment)
+        rename(src=pathfile, dst=newname)
+    
+    
 if __name__ == '__main__':
     main()
