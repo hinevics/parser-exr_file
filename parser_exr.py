41901286ef4a1ae3781fb8c0f303d7f7f64cf465
Sasha Khinevich <hinevics@gmail.com>
2021-09-25 22:52:37 +0300

    update search fun

--- parser_exr.py
+++ parser_exr.py
@@ -4,9 +4,11 @@ import os
 
 DEFAULT_PATH_FILE = r'D:\Development\Coding\parser-exr_file\CUBE___1312.exr'
 
-def search_comment(fb:bytes) -> str:
-    comment = re.search(pattern=r'comment(?P<comment>.+)comments(?P<comments>.+)compression', string=str(fb))
-    print(re.sub(string=comment.groupdict()['comment'], pattern=r'\\x00string\\x00\\t\\x00\\x00\\x00', repl=''))
+def search_comment(fb:str) -> str:
+    print(re.search(pattern=r'comment.+\\x00\\x00(?P<comment>.+)comments', string=fb).groupdict())
+    # comment = re.search(pattern=r'comment(?P<comment>.+)comments(?P<comments>.+)compression', string=str(fb))
+    # print(comment.groupdict()['comment'])
+    # print(re.sub(string=comment.groupdict()['comment'], pattern=r'(\\x00string\\x00\\t\\x00\\x00\\x00)|(\\x00string\\x00\\x0c\\x00\\x00\\x00)', repl=''))
     # print(re.sub(pattern=r'', )))
     # print(re.search(pattern=r'\\x00string\\x00\\t\\x00\\x00\\x00', string=comment.groupdict()['comment']).group())
 
@@ -22,15 +24,15 @@ def opendir() -> list:
     """
     for nf in os.listdir():
         if re.search(pattern=r'.exr$', string=nf):
-            yield nf
+            yield '{a1}\{a2}'.format(a2=nf, a1=os.getcwd())
 
 
 def main():
     # fb = openfile(path=DEFUALT_PATH_FILE)
     # search_comment(fb)
     for i in opendir():
-        with open(file=r'{a1}\{a2}'.format(a2=i, a1=os.getcwd())) as file:
-            print(file)
+        fb = openfile(path=i)
+        search_comment(fb=str(fb))
 
 
 if __name__ == '__main__':
