eb17f58a3d279842a314927c57d6c4f71e62d0a4
Sasha Khinevich <hinevics@gmail.com>
2021-09-26 01:49:19 +0300

    new ver

--- parser_exr.py
+++ parser_exr.py
@@ -1,5 +1,6 @@
 from re import search
 from os import getcwd, listdir, rename
+import argparse
 
 def search_comment(fb:str) -> str:
     """
@@ -19,18 +20,44 @@ def opendir() -> list:
     """
     the function finds all exr files in the directory where the script is located
     """
-    for nf in listdir():
-        if search(pattern=r'.exr$', string=nf):
+    fileexr = [nf for nf in listdir(path=r'{}\{}'.format(getcwd(), 'file'))
+               if search(pattern=r'.exr$', string=nf)]
+    if fileexr:
+        for nf in fileexr: 
             yield nf
+    else:
+        print('Exr file not found!')
 
-
-def main():
+def parser():
     for fileexr in opendir():
-        pathfile = r'{}\{}'.format(getcwd(), fileexr)
+        pathfile = r'{}\file\{}'.format(getcwd(), fileexr)
         fb = openfile(path=pathfile)
         comment = search_comment(fb=fb)
-        newname = r'{path}\{commet}'.format(path=getcwd(), commet=comment)
+        newname = r'{path}\file\{commet}'.format(path=getcwd(), commet=comment)
         rename(src=pathfile, dst=newname)
+        print('{} -> {}'.format(fileexr, comment))
+
+
+def parser_exr(arguments):
+    if arguments.command == 'start':
+        print('START')
+        parser()
+        print('END')
+    else:
+        print('error! restart. call the options "start"!')
+        
+def set_parser(parser):
+    parser.add_argument("-c", '--command', help="This is the 'a' variable")
+    parser.set_defaults(callback=parser_exr)
+    
+def main():
+    parser = argparse.ArgumentParser(
+        description='This program (%(prog)s) creates an Inverted Index from a set of documents',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    set_parser(parser)
+    args = parser.parse_args()
+    args.callback(args)  # callback for branches
+    
     
     
 if __name__ == '__main__':
